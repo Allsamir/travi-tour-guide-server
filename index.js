c1d2bf35c6d7e12d6ca043c3dab08913f51c13a1
Al Samir <shahinur1591@gmail.com>
2024-06-01 15:28:10 +0600

    Database setup done

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cors from "cors";
+import mongoose from "mongoose";
 const app = express();
 const PORT = process.env.PORT || 3000;
-
 app.use(
   cors({
     origin: ["http://localhost:5173"],
@@ -15,4 +15,14 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello From Assignment-12 Server");
 });
-app.listen(PORT, () => console.log("Server is Running"));
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log("Server is Running and Connected to Database"),
+    );
+  })
+  .catch((err) => {
+    console.error(err);
+    throw new Error("MongoDB Error");
+  });
