292f5214753a7ec120ca0f1dd83977c2b4b05b35
Al Samir <shahinur1591@gmail.com>
2024-06-08 00:38:07 +0600

    Booking api creating

--- controllers/bookings-controllers.js
+++ controllers/bookings-controllers.js
@@ -1,6 +1,31 @@
 import verifyUser from "../middlewares/verifyUser.js";
 import Bookings from "../models/bookings.js";
 
+export const getUsersBookigs = async (req, res) => {
+  try {
+    const { email } = req.query;
+    verifyUser(req.user.email, email);
+    const bookings = await Bookings.aggregate([
+      { $match: { email: email } },
+      {
+        $lookup: {
+          from: "packages",
+          localField: "packageID",
+          foreignField: "_id",
+          as: "packageDetails",
+        },
+      },
+      {
+        $unwind: "$packageDetails",
+      },
+    ]);
+    console.log(bookings);
+    res.status(200).send(bookings);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const createNewBookings = async (req, res) => {
   try {
     const { email, name, photoURL, price, date, guide, status, packageID } =
--- routes/booking.routes.js
+++ routes/booking.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
-import { createNewBookings } from "../controllers/bookings-controllers.js";
+import {
+  createNewBookings,
+  getUsersBookigs,
+} from "../controllers/bookings-controllers.js";
 import verifyToken from "../middlewares/verifyToken.js";
 
 const bookingsRoute = Router();
 
+bookingsRoute.get("/", verifyToken, getUsersBookigs);
 bookingsRoute.post("/", verifyToken, createNewBookings);
 
 export default bookingsRoute;
