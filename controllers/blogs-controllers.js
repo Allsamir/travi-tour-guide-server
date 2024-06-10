d0e40cb7eab42ba8fd4d0148f028f6b9f75035db
Al Samir <shahinur1591@gmail.com>
2024-06-10 18:44:10 +0600

    Deployment done

--- controllers/blogs-controllers.js
+++ controllers/blogs-controllers.js
@@ -1,5 +1,24 @@
 import Blog from "../models/blog.js";
 
+export const getAllTheBlogs = async (req, res) => {
+  try {
+    const blogs = await Blog.find();
+    res.status(200).send(blogs);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getSingleBlog = async (req, res) => {
+  try {
+    const { id } = req.query;
+    const blog = await Blog.findById({ _id: id });
+    res.status(200).send(blog);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 export const createABlog = async (req, res) => {
   try {
     const newBlog = new Blog(req.body);
--- routes/blogs-routes.js
+++ routes/blogs-routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import verifyToken from "../middlewares/verifyToken.js";
 import verifyAdmin from "../middlewares/verifyAdmin.js";
-import { createABlog } from "../controllers/blogs-controllers.js";
+import {
+  createABlog,
+  getAllTheBlogs,
+  getSingleBlog,
+} from "../controllers/blogs-controllers.js";
 
 const blogRoutes = Router();
 
+blogRoutes.get("/", getAllTheBlogs);
+blogRoutes.get("/singleBlog", getSingleBlog);
 blogRoutes.post("/", verifyToken, verifyAdmin, createABlog);
 
 export default blogRoutes;
